c28ce0cadeb1e4ba2bfc9dc148c0cd15b77d7de6
Miloud <basicalllymiloud@gmail.com>
2026-05-25 11:05:59 +0100

    Update finalserver.c

--- finalserver.c
+++ finalserver.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
+
 #define MAX_CLIENTS 10
 
 typedef struct {
@@ -50,7 +51,6 @@ void *handle_client(void *arg) {
   char buffer[1024];
   char username[50];
 
-  // Accept the username
   ssize_t bytes_received;
   bytes_received =
       recv(my_client.client_socket, username, sizeof(username) - 1, 0);
@@ -62,7 +62,6 @@ void *handle_client(void *arg) {
 
   username[bytes_received] = '\0';
 
-  // Update the username in the client_list
   for (int i = 0; i < MAX_CLIENTS; i++) {
     if (client_list[i].client_socket == my_client.client_socket) {
       strcpy(client_list[i].username, username);
@@ -82,7 +81,6 @@ void *handle_client(void *arg) {
     }
     buffer[bytes_received] = '\0';
 
-    // Parse time
     struct tm *ptr;
     time_t t = time(NULL);
     ptr = localtime(&t);
@@ -96,7 +94,6 @@ void *handle_client(void *arg) {
     while ((end = strchr(msg, '\n'))) {
       *end = '\0';
 
-      // Remove carriage return if present
       int len = strlen(msg);
       if (len > 0 && msg[len - 1] == '\r') {
         msg[len - 1] = '\0';
@@ -129,117 +126,4 @@ void *handle_client(void *arg) {
 
         char footer[100];
         snprintf(footer, sizeof(footer),
-                 "==================\nTotal: %d users online\n\n", user_count);
-        strcat(online_users, footer);
-
-        printf("Sending list:\n%s", online_users);
-
-        if (write(my_client.client_socket, online_users, strlen(online_users)) <
-            0) {
-          perror("Error sending online users list");
-          break_outer_loop = true;
-          break;
-        }
-
-        printf("Successfully sent online users list to %s\n", username);
-      } else {
-        // Regular message
-        printf("[\033[1;34m%s\033[0m] \033[1;35m%s\033[0m: %s\n", timestamp,
-               username, msg);
-        char new_msg[2048];
-        memset(new_msg, 0, sizeof(new_msg));
-        msg_formatter(new_msg, sizeof(new_msg), timestamp, msg, username);
-
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-          if (client_list[i].client_socket != 0 &&
-              client_list[i].client_socket != my_client.client_socket) {
-            if (write(client_list[i].client_socket, new_msg, strlen(new_msg)) <
-                0) {
-              perror("sending message failed");
-              client_list[i].client_socket = 0;
-              client_list[i].is_active = false;
-            }
-          }
-        }
-      }
-
-      msg = end + 1;
-      write(my_client.client_socket, "Ok\n", 3);
-    }
-
-    if (break_outer_loop)
-      break;
-  }
-
-  empty_client_fds(client_list, my_client);
-  close(my_client.client_socket);
-  return NULL;
-}
-
-int main() {
-  client_init(client_list);
-
-  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket < 0) {
-    perror("socket() failed");
-    return 1;
-  }
-
-  // Allow reuse of address
-  int opt = 1;
-  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-  struct sockaddr_in server_info = {.sin_family = AF_INET,
-                                    .sin_port = htons(2000),
-                                    .sin_addr.s_addr = INADDR_ANY};
-
-  if (bind(server_socket, (struct sockaddr *)&server_info,
-           sizeof(server_info)) < 0) {
-    perror("bind() failed");
-    close(server_socket);
-    return 1;
-  }
-
-  if (listen(server_socket, MAX_CLIENTS) < 0) {
-    perror("listen() failed");
-    close(server_socket);
-    return 1;
-  }
-
-  printf("Server listening on port 2000...\n");
-
-  struct sockaddr_in client_info;
-  socklen_t client_len = sizeof(client_info);
-
-  while (1) {
-    client *my_client = (client *)malloc(sizeof(client));
-    my_client->client_socket =
-        accept(server_socket, (struct sockaddr *)&client_info, &client_len);
-
-    if (my_client->client_socket == -1) {
-      perror("accept() failed");
-      free(my_client);
-      continue;
-    }
-
-    printf("Connection succeeded\n");
-    printf("Client connected from %s:%d\n", inet_ntoa(client_info.sin_addr),
-           ntohs(client_info.sin_port));
-
-    // Add client to list
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-      if (client_list[i].client_socket == 0) {
-        client_list[i].client_socket = my_client->client_socket;
-        client_list[i].is_active = true;
-        break;
-      }
-    }
-
-    pthread_t thread_id;
-    pthread_create(&thread_id, NULL, handle_client, my_client);
-    pthread_detach(thread_id);
-  }
-
-  close(server_socket);
-  return 0;
-}
+                 "==================\nTotal: %d
