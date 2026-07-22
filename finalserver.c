05891560f9bd466708247148821c710fe691e021
Miloud-ux <basicalllymiloud@gmail.com>
2026-07-22 19:10:15 +0100

    fixed parsing issue with the end command and chose arbitrary port number for both client and server

--- finalserver.c
+++ finalserver.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #define MAX_CLIENTS 10
+#define PORT 8080
 
 typedef struct {
   int client_socket;
@@ -47,10 +48,8 @@ void msg_formatter(char new_msg[], size_t new_msg_size, const char timestamp[],
 void *handle_client(void *arg) {
   client my_client = *(client *)arg;
   free(arg);
-
   char buffer[1024];
   char username[50];
-
   ssize_t bytes_received;
   bytes_received =
       recv(my_client.client_socket, username, sizeof(username) - 1, 0);
@@ -59,18 +58,16 @@ void *handle_client(void *arg) {
     empty_client_fds(client_list, my_client);
     return NULL;
   }
-
   username[bytes_received] = '\0';
-
   for (int i = 0; i < MAX_CLIENTS; i++) {
     if (client_list[i].client_socket == my_client.client_socket) {
-      strcpy(client_list[i].username, username);
+      strncpy(client_list[i].username, username,
+              sizeof(client_list[i].username) - 1);
+      client_list[i].username[sizeof(client_list[i].username) - 1] = '\0';
       break;
     }
   }
-
   printf("\033[1;32m%s has connected\033[0m\n", username);
-
   bool break_outer_loop = false;
   while (1) {
     bytes_received =
@@ -80,27 +77,21 @@ void *handle_client(void *arg) {
       break;
     }
     buffer[bytes_received] = '\0';
-
     struct tm *ptr;
     time_t t = time(NULL);
     ptr = localtime(&t);
     char timestamp[20];
     strftime(timestamp, sizeof(timestamp), "%H:%M:%S", ptr);
-
     char *msg;
     char *end;
     msg = buffer;
-
     while ((end = strchr(msg, '\n'))) {
       *end = '\0';
-
       int len = strlen(msg);
       if (len > 0 && msg[len - 1] == '\r') {
         msg[len - 1] = '\0';
       }
-
       printf("DEBUG: Received message: '%s'\n", msg);
-
       if (strcmp(msg, ":end") == 0) {
         printf("\033[1;31m%s disconnected\033[0m\n", username);
         fflush(stdout);
@@ -109,10 +100,8 @@ void *handle_client(void *arg) {
       } else if (strcmp(msg, ":online") == 0) {
         printf("=== ONLINE COMMAND DETECTED ===\n");
         printf("User %s requested online users list\n", username);
-
         char online_users[2048] = "\n=== Online Users ===\n";
         int user_count = 0;
-
         for (int i = 0; i < MAX_CLIENTS; i++) {
           if (client_list[i].is_active && strlen(client_list[i].username) > 0) {
             user_count++;
@@ -123,7 +112,98 @@ void *handle_client(void *arg) {
             printf("Added user: %s\n", client_list[i].username);
           }
         }
-
         char footer[100];
         snprintf(footer, sizeof(footer),
-                 "==================\nTotal: %d
+                 "==================\nTotal: %d users online\n", user_count);
+        strcat(online_users, footer);
+        send(my_client.client_socket, online_users, strlen(online_users), 0);
+        msg = end + 1;
+        continue;
+      } else {
+        char formatted_msg[1200];
+        msg_formatter(formatted_msg, sizeof(formatted_msg), timestamp, msg,
+                      username);
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+          if (client_list[i].is_active &&
+              client_list[i].client_socket != my_client.client_socket) {
+            send(client_list[i].client_socket, formatted_msg,
+                 strlen(formatted_msg), 0);
+          }
+        }
+        printf("%s", formatted_msg);
+      }
+      msg = end + 1;
+    }
+    if (break_outer_loop) {
+      break;
+    }
+  }
+  close(my_client.client_socket);
+  empty_client_fds(client_list, my_client);
+  return NULL;
+}
+
+int main(void) {
+  client_init(client_list);
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket < 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+  int opt = 1;
+  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(PORT);
+  if (bind(server_socket, (struct sockaddr *)&server_addr,
+           sizeof(server_addr)) < 0) {
+    perror("bind failed");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+  }
+  if (listen(server_socket, MAX_CLIENTS) < 0) {
+    perror("listen failed");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+  }
+  printf("server listening on port %d\n", PORT);
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int client_socket =
+        accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+    if (client_socket < 0) {
+      perror("accept failed");
+      continue;
+    }
+    int slot = -1;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+      if (!client_list[i].is_active) {
+        slot = i;
+        break;
+      }
+    }
+    if (slot == -1) {
+      printf("max clients reached, rejecting connection\n");
+      close(client_socket);
+      continue;
+    }
+    client_list[slot].client_socket = client_socket;
+    client_list[slot].is_active = true;
+    client *arg = malloc(sizeof(client));
+    *arg = client_list[slot];
+    pthread_t tid;
+    if (pthread_create(&tid, NULL, handle_client, arg) != 0) {
+      perror("pthread_create failed");
+      close(client_socket);
+      client_list[slot].is_active = false;
+      free(arg);
+      continue;
+    }
+    pthread_detach(tid);
+  }
+  close(server_socket);
+  return 0;
+}
